ed37565553b6ff6c531212c2246d26ab9ee20fcb
Gabriel Ferreira <hi.gabrielferreira@gmail.com>
2019-02-05 23:53:09 -0200

    migrate to monorepo

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,18 +1,18 @@
-import express from "express";
-import bodyParser from "body-parser";
-import routes from "./app/routes";
+import express from 'express';
+import bodyParser from 'body-parser';
+import routes from './app/routes';
 // import { User } from "./app/models";
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+	bodyParser.urlencoded({
+		extended: true
+	})
 );
-app.use("/", routes);
+app.use('/', routes);
 
 // app.get("/", (request, response) => {
 //   response.json({ info: "Node.js, Express, and Postgres API" });
@@ -28,5 +28,5 @@ app.use("/", routes);
 // });
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
+	console.log(`App running on port ${port}.`);
 });
