c6b09a03980e6cd34d126815f8fcee6bd7d3380c
Gabriel Ferreira <hi.gabrielferreira@gmail.com>
2019-02-09 01:30:00 -0200

    add compression to server

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import compression from 'compression';
 import bodyParser from 'body-parser';
 import routes from './app/routes';
 
 const app = express();
 const port = 4000;
 
+app.use(compression());
 app.use(bodyParser.json());
 app.use(
 	bodyParser.urlencoded({
